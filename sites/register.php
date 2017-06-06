2322d89ff37c864418227d1fdeb5ec057d25bdf4
eddsus <edd.sustr@gmail.com>
2017-06-06 12:08:57 +0200

    registration site

--- sites/register.php
+++ sites/register.php
@@ -1,158 +1,136 @@
-<?php
-include '../inc/head.php';
-include '../inc/navigation.php';
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-//Variablen anlegen
-$Anrede = null;
-$Vorname = null;
-$Nachname = null;
-$Adresse = null;
-$PLZ = null;
-$Ort = null;
-$User = null;
-$Pwd = null;
-$Pwd_confirm = null;
-$Email = null;
-$err = false;
+<h2>User Registration</h2>
 
-//Überprüfen, ob Formular abgesendet wurde
-if (isset($_GET['submit'])) {
-
-    //Einzelne GET-Parameter prüfen, ob diese gesetzt sind bzw. ein Leer-String und Wert in Variable schreiben
-    //Im Fehlerfall wird die Variable $err auf true gesetzt
-    if (!empty($_GET['Anrede'])) {
-        switch($_GET['Anrede']){
-            case 'f': $Anrede = "Frau"; break;
-            case 'h': $Anrede = "Herr"; break;
+<form class="form-horizontal" action="registration.php" method="post">
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Gender</label>
+        <div class="col-sm-10">
+            <select name="gender" class="form-control" id="gender" required>
+                <option value="">Please choose your gender</option>
+                <option >Male</option>
+                <option >Female</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Your name</label>
+        <div class="col-sm-10">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Martin" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Your surname</label>
+        <div class="col-sm-10">
+            <input type="text" name="surname" class="form-control" id="surname" placeholder="Musterman" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Email</label>
+        <div class="col-sm-10">
+            <input type="email" name="email" class="form-control" id="email" placeholder="me@home.com" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Your address</label>
+        <div class="col-sm-10">
+            <input type="text" name="address" class="form-control" id="address" placeholder="Höchstädtplatz 6" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">ZIP</label>
+        <div class="col-sm-10">
+            <input type="text" name="zip" class="form-control" id="zip" placeholder="1200" pattern="\d{4,}" title="At least 4 numbers." required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">City</label>
+        <div class="col-sm-10">
+            <input type="text" name="city" class="form-control" id="ort" placeholder="Wien" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Username</label>
+        <div class="col-sm-10">
+            <input type="text" name="username" class="form-control" id="username" placeholder="Username" required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Password</label>
+        <div class="col-sm-10">
+            <input type="password" name="password" class="form-control" id="password" placeholder="Your password" 
+                   pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" 
+                   title="Password has to contain at least 8 characters with 1 lower letter, 1 upper letter and 1 number." required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2 control-label">Confirm your password</label>
+        <div class="col-sm-10">
+            <input type="password" name="confirmPassword" class="form-control" id="confirmPassword" placeholder="Your password">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" name="submit" class="btn btn-default" onclick="return validate()">Create account</button>
+        </div>
+    </div>
+</form>
+<!-- password match check !-->
+<script type="text/javascript">
+    function validate() {
+        var password = document.getElementById("password").value;
+        var confirmPassword = document.getElementById("confirmPassword").value;
+        if (password != confirmPassword) {
+            alert("Passwords do not match.");
+            return false;
         }
+        return true;
+    }
+</script>
 
-    } else
-        $err = true;
 
-    if (!empty($_GET['Vorname'])) {
-        $Vorname = $_GET['Vorname'];
-    } else
-        $err = true;
+<?php
 
-    if (!empty($_GET['Nachname'])) {
-        $Nachname = $_GET['Nachname'];
-    } else
-        $err = true;
-
-    if (!empty($_GET['Adresse'])) {
-        $Adresse = $_GET['Adresse'];
-    } else
-        $err = true;
-
-    if (!empty($_GET['PLZ'])) {
-        $PLZ = $_GET['PLZ'];
-    } else
-        $err = true;
-
-    if (!empty($_GET['Ort'])) {
-        $Ort = $_GET['Ort'];
-    } else
-        $err = true;
-
-    if (!empty($_GET['User'])) {
-        $User = $_GET['User'];
-    } else
-        $err = true;
-
-    if (!empty($_GET['Pwd']) && !empty($_GET['Pwd_confirm'])) {
-        $Pwd = $_GET['Pwd'];
-    } else
-        $err = true;
-
-    if (!empty($_GET['Pwd_confirm'])) {
-        $Pwd_confirm = $_GET['Pwd_confirm'];
-    } else
-        $err = true;
-
-    if($Pwd != $Pwd_confirm){
-        $err = true;
+    $gender=null;
+    $name=null;
+    $surname=null;
+    $email=null;
+    $address=null;
+    $zip=null;
+    $city=null;
+    $username=null;
+    $password=null;
+    
+if (isset($_POST['submit'])) {
+    if (isset($_POST['gender'])) {
+        $gender = $_POST['gender'];
+    }
+    if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+    }
+        if (isset($_POST['surname'])) {
+        $surname = $_POST['surname'];
+    }
+        if (isset($_POST['email'])) {
+        $email = $_POST['email'];
+    }
+        if (isset($_POST['address'])) {
+        $address = $_POST['address'];
+    }
+        if (isset($_POST['zip'])) {
+        $zip = $_POST['zip'];
+    }
+        if (isset($_POST['city'])) {
+        $city = $_POST['city'];
+    }
+        if (isset($_POST['username'])) {
+        $username = $_POST['username'];
+    }
+        if (isset($_POST['password'])) {
+        $password = $_POST['password'];
     }
 
-    if (!empty($_GET['Email'])) {
-        $Email = $_GET['Email'];
-    } else
-        $err = true;
 
-    //wenn alle Daten vorhanden sind und kein Fehler aufgetreten ist, wird neues User-Objekt erzeugt
-    if ($err == false) {
-        $user = new User(0,$Anrede,$Vorname,$Nachname,$Adresse,$PLZ,$Ort,$User,$Pwd,$Email);
-        //user-Objekt wird in registerUser-Funktion der DB-Klasse übergeben
-        $db->registerUser($user);
-    }else{
-        echo "Bitte füllen Sie alle Felder korrekt aus!";
-    }
-}
-?>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>formular</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    </head>
-    <body>
-        <form method="GET">
-            <div class="form-group">
-                <label>Gender</label>
-                <select name="gender" class="form-control" required >
-                    <option value="">Please choose your gender</option>
-                    <option>Male</option>
-                    <option>Female</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <input type="text" name="name" placeholder="Your name" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <input type="text" name="surname" placeholder="Your surname" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <input type="email" name="email" placeholder="Your email adress"class="form-control" required>
-            </div>
-            <div class="form-group">
-                <input type="text" name="street" placeholder="Your street" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <input type="text" name="zip" placeholder="Your ZIP" class="form-control" required pattern="(?=.*?[0-9]).{4,}" title="At least 4 numbers.">
-            </div>
-            <div class="form-group">
-                <input type="text" name="city" placeholder="Your city" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <input type="text" name="username" placeholder="Your username" class="form-control" required>
-            </div>
-
-            <div class="form-group">
-                <input type="password" name="password" id="password" placeholder="Your password" class="form-control" 
-                       required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" 
-                       title="Password has to contain at least 8 characters with 1 lower letter, 1 upper letter and 1 number.">
-            </div>
-            <div class="form-group">
-                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm your password" class="form-control" required>
-            </div>
-            <button type="submit" class="btn btn-primary" onclick="return validate()">Create account</button>
-        </form>
-        
-        <!-- password match check !-->
-        <script type="text/javascript">
-            function validate() {
-                var password = document.getElementById("password").value;
-                var confirmPassword = document.getElementById("confirmPassword").value;
-                if (password != confirmPassword) {
-                    alert("Passwords do not match.");
-                    return false;
-                }
-                return true;
-            }
-        </script>
-
-    </body>
-</html>
-
-
+    
+} 
